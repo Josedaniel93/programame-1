6f95df74c8e1a2bf045762980fb459609af80948
Josedaniel93 <55553800+Josedaniel93@users.noreply.github.com>
2019-10-08 10:08:09 +0200

    P362 updated

--- p362.java
+++ p362.java
@@ -1,17 +1,32 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class p362 {
-	
+
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
-		
-		int ncasos = sc.nextInt();
+
+		int ncasos;
+		ncasos = sc.nextInt();
 		int dia;
 		int mes;
 
-		for(int i=0; i<ncasos; i++){
+		for(int i=1; i<=ncasos; i++){
+			dia = sc.nextInt();	
+			mes = sc.nextInt();	
+
+	
+		if(dia == 25){
 			
-			
+		    if (mes == 12){
+				System.out.println("SI");
+				
+		    } else {
+				System.out.println("NO");
+				
+			}
+		} else {
+			System.out.println("NO");
+		}
 		}
 
 	}
